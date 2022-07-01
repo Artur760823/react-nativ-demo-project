80a759d22d51f4f3b58f1fe9a49da09c36b258f6
Artur <artur.zajaczkowski76@gmail.com>
2022-07-01 13:37:29 +0200

    Falt list

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+  FlatList,
+} from "react-native";
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState("");
@@ -13,29 +21,42 @@ export default function App() {
   function addGoalHandler() {
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals,
-      enteredGoalText,
+      { text: enteredGoalText, key: Math.random().toString() },
     ]);
   }
 
   return (
     <View style={styles.appContainer}>
+      <View>
+        <Text style={styles.appTitle}>Shopping List</Text>
+      </View>
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.textInput}
           placeholder="Your coutse goal"
           onChangeText={goalInputHandler}
         />
-        <Button color={"#728FCE"} title="Add Goal" onPress={addGoalHandler} />
+        <Button
+          color={"#728FCE"}
+          title="Add Expense"
+          onPress={addGoalHandler}
+        />
       </View>
       <View style={styles.goalsContainer}>
-        {courseGoals.map((goal, index) => (
-          <View key={index} style={styles.goalList}>
-            <Text style={styles.goalItem}>
-              {" "}
-              {index + 1} - {goal}
-            </Text>
-          </View>
-        ))}
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.goalList}>
+                <Text style={styles.goalItem}>{itemData.item.text}</Text>
+              </View>
+            );
+          }}
+          alwaysBounceVertical={false}
+          keyExtractor={(item, index)=>{
+            return item.key;
+          }}
+        />
       </View>
     </View>
   );
@@ -46,6 +67,13 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 50,
     paddingHorizontal: 16,
+  },
+  appTitle: {
+    textAlign: "center",
+    paddingTop: 20,
+    paddingBottom: 10,
+    fontSize: 25,
+    fontWeight: "bold",
   },
   inputContainer: {
     flex: 1,
@@ -59,7 +87,7 @@ const styles = StyleSheet.create({
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
-    width: "70%",
+    width: "65%",
     marginRight: 8,
     padding: 8,
   },
@@ -72,7 +100,7 @@ const styles = StyleSheet.create({
     borderRadius: 6,
     backgroundColor: "#4863A0",
   },
-  goalItem:{
+  goalItem: {
     color: "white",
-  }
+  },
 });
